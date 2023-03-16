fcc1013c27c94d68ff8f104378c820025106d34c
Bryan Chavez <bryanchavez102@gmail.com>
2023-03-16 13:52:05 -0400

    verification

--- Portfolio_BackEnd/src/main/java/com/example/demo/PortfolioBackEndApplication.java
+++ Portfolio_BackEnd/src/main/java/com/example/demo/PortfolioBackEndApplication.java
@@ -13,20 +13,20 @@ public class PortfolioBackEndApplication {
 
 	public static void main(String[] args) {
 		
-//		 Map<String, String> env_var = System.getenv();
-//
-//	        // Loop through all environment variables
-//	        for (String envName : env_var.keySet()) {
-//	            // Print environment variable name and value to console
-//	            System.out.format("%s = %s", envName, env_var.get(envName));
-//	            System.out.println();
-//	        }
-//		
-//		
-//		String fromUser= System.getenv("PORTFOLIO_GMAIL_USERNAME");
-//		String fromUserPassword= System.getenv("PORTFOLIO_GMAIL_PASSWORD");
-//		System.out.println("fromUser: "+ fromUser);
-//		System.out.println("fromUserPassword: "+ fromUserPassword);
+		 Map<String, String> env_var = System.getenv();
+
+	        // Loop through all environment variables
+	        for (String envName : env_var.keySet()) {
+	            // Print environment variable name and value to console
+	            System.out.format("%s = %s", envName, env_var.get(envName));
+	            System.out.println();
+	        }
+		
+		
+		String fromUser= System.getenv("PORTFOLIO_GMAIL_USERNAME");
+		String fromUserPassword= System.getenv("PORTFOLIO_GMAIL_PASSWORD");
+		System.out.println("fromUser: "+ fromUser);
+		System.out.println("fromUserPassword: "+ fromUserPassword);
 		
 		
 		SpringApplication.run(PortfolioBackEndApplication.class, args);
